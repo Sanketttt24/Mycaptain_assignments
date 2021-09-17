e950a1d7afe1bcd480ddbd5e6fb0f919903c850b
Sanketttt24 <89469334+Sanketttt24@users.noreply.github.com>
2021-09-17 21:17:54 +0530

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,10 @@
+# Python program to print positive Numbers in a List
+
+#2
+list2 = [12, 14, -95, 3]
+# iterating each number in list
+for num in list2:
+
+    # checking condition
+    if num >= 0:
+        print(num, end=" ")
